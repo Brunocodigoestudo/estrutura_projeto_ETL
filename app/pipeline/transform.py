1220418b5b38404441d5dbdbc12f7f0cdb0d57d4
Brunocodigoestudo <brunoeng.proj@hotmail.com>
2024-02-02 03:10:52 -0300

    test atualizado

--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -2,11 +2,10 @@ from typing import List
 
 import pandas as pd
 
-"""
-função para transformar uma lista de dataframe em um único dataframe
-
-"""
-
 
 def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    """
+    função para transformar uma lista de dataframe em um único dataframe
+    """
+
     return pd.concat(data_frame_list)
--- teste/test_pipeline.py
+++ teste/test_pipeline.py
@@ -1,9 +1,30 @@
+# Importe os módulos necessários
 import pandas as pd
 
+from app.pipeline.transform import concat_data_frames
 
-def testar_concatenacao_lista_df():
-    arrange = 3
 
-    act = 3
+def testar_a_concatenacao_da_lista_de_dataframe():
+    """Use o arrange, act e assert para testar a função concat_data_frames"""
 
-    assert arrange == act
+    # Arrange
+    # Defina ou importe os DataFrames que serão usados nos testes
+    df_1 = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
+    df_2 = pd.DataFrame({"col1": [5, 6], "col2": [7, 8]})
+
+    data_frame_list = [df_1, df_2]
+    data_frame = pd.concat(data_frame_list, ignore_index=True)
+
+    # Act
+    df = concat_data_frames(data_frame_list)
+
+    # Assert
+    assert df.shape == (4, 2)
+
+    # Redefina os índices
+    df.reset_index(drop=True, inplace=True)
+    data_frame.reset_index(drop=True, inplace=True)
+
+    # Compare cada elemento individualmente
+    for col in df.columns:
+        assert all(df[col] == data_frame[col])
