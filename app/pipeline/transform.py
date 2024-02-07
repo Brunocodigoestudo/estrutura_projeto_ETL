1645d45ca0d08c1aa380cf06d425db4486b9c6b9
Brunocodigoestudo <brunoeng.proj@hotmail.com>
2024-02-07 11:02:58 -0300

    atualizando hj

--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
     """
-    função para transformar uma lista de dataframe em um único dataframe
+    Função para transformar uma lista de dataframes em um único dataframe
     """
 
     return pd.concat(data_frame_list)
